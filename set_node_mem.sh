353561ba2e6edbbe39f102054157af03da83a0ac
Chris Tran <6634746+sYnVerse@users.noreply.github.com>
2026-06-06 23:53:25 -0700

    Set NODE_OPTIONS max-old-space-size to 4096 MB

--- set_node_mem.sh
+++ set_node_mem.sh
@@ -1,5 +1,5 @@
 memory=$(awk '/MemTotal/{print $2}' /proc/meminfo);
 memory=$((memory / 1024));
-node_options="--max-old-space-size=$((memory - 1024))";
+node_options="--max-old-space-size=4096";
 echo "NODE_OPTIONS=$node_options";
 export NODE_OPTIONS="$node_options";
